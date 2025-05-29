02e8f652fe364e03aceec18704e53d5d957afe21
Mitchell R. Vollger <mvollger@gmai.com>
2025-05-29 08:12:25 +0200

    update

--- src/bin/fiber-pipeline.rs
+++ src/bin/fiber-pipeline.rs
@@ -6,6 +6,7 @@ use std::io::copy;
 use std::process::{Child, Command, Stdio}; // Add methods on commands
 // add anyhow for error handling
 use anyhow::{Error, Result};
+use std::os::unix::prelude::*;
 
 #[derive(Parser, Debug)]
 /// Execute a complex pipeline involving multiple tools.
@@ -80,31 +81,10 @@ pub fn run_vg_surject(vg_inject: &mut Child, args: &FiberPipelineArgs) -> Child 
         .expect("Failed to start vg surject")
 }
 
-fn main() -> Result<(), anyhow::Error> {
-    let args = FiberPipelineArgs::parse();
-
-    // Set log level to info
-    env_logger::Builder::new()
-        .filter(None, log::LevelFilter::Info)
-        .init();
-
-    log::info!(
-        "Starting pipeline with input: {} and output: {}",
-        args.input,
-        args.output
-    );
-
-    // Step 1: change-pan-spec
-    let mut change_pan_spec = run_change_pan_spec(&args);
-
-    // Step 2: vg inject
-    let mut vg_inject = run_vg_inject(&mut change_pan_spec, &args);
-
-    // Step 3: vg surject
-    let mut vg_surject = run_vg_surject(&mut vg_inject, &args);
-
+pub fn run_second_change_pan_spec(vg_surject: &mut Child, args: &FiberPipelineArgs) -> Child {
     // Step 4: change-pan-spec
-    let mut change_pan_spec_2 = Command::cargo_bin("change-pan-spec")?
+    Command::cargo_bin("change-pan-spec")
+        .expect("change-pan-spec binary not found")
         .args(&["-t", &args.threads.to_string(), "-u"])
         .stdin(
             vg_surject
@@ -114,10 +94,12 @@ fn main() -> Result<(), anyhow::Error> {
         )
         .stdout(Stdio::piped())
         .spawn()
-        .expect("Failed to start second change-pan-spec");
+        .expect("Failed to start second change-pan-spec")
+}
 
+pub fn run_samtools_sort(change_pan_spec_2: &mut Child, args: &FiberPipelineArgs) -> Child {
     // Step 5: samtools sort -N -u
-    let mut samtools_sort_1 = Command::new("samtools")
+    Command::new("samtools")
         .args(&["sort", "-N", "-u", "-@", &args.threads.to_string()])
         .stdin(
             change_pan_spec_2
@@ -127,13 +109,18 @@ fn main() -> Result<(), anyhow::Error> {
         )
         .stdout(Stdio::piped())
         .spawn()
-        .expect("Failed to start samtools sort -N -u");
+        .expect("Failed to start samtools sort -N -u")
+}
 
+pub fn run_sync_tags(samtools_sort_1: &mut Child, args: &FiberPipelineArgs) -> Child {
     // Step 6: sync-tags
-    let mut sync_tags = Command::cargo_bin("sync-tags")?
+    Command::cargo_bin("sync-tags")
+        .expect("sync-tags binary not found")
         .args(&[
+            "-t",
+            &args.threads.to_string(),
             "-",
-            &format!("<(samtools sort -N -u -@ {} {})", args.threads, args.input),
+            format!("samtools sort -N -u -@ {} {}", args.threads, &args.input).as_str(),
         ])
         .stdin(
             samtools_sort_1
@@ -143,10 +130,12 @@ fn main() -> Result<(), anyhow::Error> {
         )
         .stdout(Stdio::piped())
         .spawn()
-        .expect("Failed to start sync-tags");
+        .expect("Failed to start sync-tags")
+}
 
+pub fn run_final_samtools_sort(sync_tags: &mut Child, args: &FiberPipelineArgs) -> Child {
     // Step 7: samtools sort --write-index
-    let mut samtools_sort_2 = Command::new("samtools")
+    Command::new("samtools")
         .args(&[
             "sort",
             "-@",
@@ -163,7 +152,60 @@ fn main() -> Result<(), anyhow::Error> {
         )
         .stdout(Stdio::inherit())
         .spawn()
-        .expect("Failed to start samtools sort --write-index");
+        .expect("Failed to start samtools sort --write-index")
+}
+
+fn main() -> Result<(), anyhow::Error> {
+    let args = FiberPipelineArgs::parse();
+
+    // Set log level to info
+    env_logger::Builder::new()
+        .filter(None, log::LevelFilter::Info)
+        .init();
+    log::info!(
+        "Starting pipeline with input: {} and output: {}",
+        args.input,
+        args.output
+    );
+
+    // check and make sure samtools and vg are installed
+    Command::new("samtools")
+        .arg("--version")
+        .output()
+        .expect("samtools is not installed or not found in PATH");
+    Command::new("vg")
+        .arg("--version")
+        .output()
+        .expect("vg is not installed or not found in PATH");
+    // ensure the input file path exists and that it is not a pipe
+    let in_path = std::path::Path::new(&args.input);
+    if !in_path.exists() {
+        return Err(Error::msg(format!(
+            "Input file does not exist: {}",
+            args.input
+        )));
+    }
+
+    // Step 1: change-pan-spec
+    let mut change_pan_spec = run_change_pan_spec(&args);
+
+    // Step 2: vg inject
+    let mut vg_inject = run_vg_inject(&mut change_pan_spec, &args);
+
+    // Step 3: vg surject
+    let mut vg_surject = run_vg_surject(&mut vg_inject, &args);
+
+    // Step 4: change-pan-spec
+    let mut change_pan_spec_2 = run_second_change_pan_spec(&mut vg_surject, &args);
+
+    // Step 5: samtools sort -N -u
+    let mut samtools_sort_1 = run_samtools_sort(&mut change_pan_spec_2, &args);
+
+    // Step 6: sync-tags
+    let mut sync_tags = run_sync_tags(&mut samtools_sort_1, &args);
+
+    // Step 7: samtools sort --write-index
+    let mut samtools_sort_2 = run_final_samtools_sort(&mut sync_tags, &args);
 
     // Wait for all commands to finish
     change_pan_spec.wait().expect("change-pan-spec failed");
